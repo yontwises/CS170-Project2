2c1fc5b9285f2dad8e838d7e8aadb7d0f0de1278
Yontwises <kenny.yontwises@gmail.com>
2021-12-10 15:49:36 -0800

    Forward Selection WIP

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <sstream>
 #include <limits>
 #include <cmath>
+#include <algorithm>
 
 using namespace std; 
 
 //compute the distance of points from training set and then return the class label with the least distance away
-int nearestNeighborClassifier(vector<vector<double>> data, int dont_compare ) {
+int nearestNeighborClassifier(vector<vector<double>> data, int dont_compare, vector<int> features ) {
     int nearestNeighbor = 0; 
     double shortestDistance = std::numeric_limits<double>::max();
     double distance = 0.0;
@@ -23,8 +24,8 @@ int nearestNeighborClassifier(vector<vector<double>> data, int dont_compare ) {
             distance = 0; 
             // cout << data.at(i).at(j) << endl << data.at(dont_compare).at(j) << endl;
 
-            for (int j = 1; j < data.at(0).size(); j++) {
-                distance = distance + pow(data.at(i).at(j) - data.at(dont_compare).at(j),2);
+            for (int j = 0; j < features.size(); j++) {
+                distance = distance + pow(data.at(i).at(features.at(j)) - data.at(dont_compare).at(features.at(j)),2);
                 // cout << i << ":" << data.at(i).at(j) << endl << dont_compare << ":" << data.at(dont_compare).at(j) << endl;
                 
             }
@@ -38,12 +39,12 @@ int nearestNeighborClassifier(vector<vector<double>> data, int dont_compare ) {
     return nearestNeighbor;
 }
 
-double leave_one_out_cross_validation(vector<vector<double>> data) {
+double leave_one_out_cross_validation(vector<vector<double>> data,vector<int> features) {
     double correct = 0.0; 
     int neighbor = 0;
     int accuracy = 0;
     for(int i = 0; i < data.size();i++) {
-        neighbor = nearestNeighborClassifier(data,i);
+        neighbor = nearestNeighborClassifier(data,i,features);
         // cout << "Object " << i + 1 << " is class " << data.at(i).at(0) << endl;
         // cout << "Its nearest neighbor is " << neighbor + 1 << "which is in class " << data.at(neighbor).at(0) << endl;
         if(data.at(i).at(0) == data.at(neighbor).at(0)) {
@@ -53,6 +54,37 @@ double leave_one_out_cross_validation(vector<vector<double>> data) {
     accuracy = (correct / data.size()) * 100;
     return accuracy;
 }
+
+void forward_Selection(vector<vector<double>> data) {
+    vector<int> current_set_of_features;   
+    vector<int> final_set;
+    double accuracy = 0.0; 
+
+    for (int i = 0;i < data.at(0).size();i++) {
+        //start with an empty set and then add features if it as the highest accuracy
+        int feature_to_add_at_this_level; 
+        int local_add;
+        double best_so_far_accuracy = 0; 
+
+        for(int k = 1; k < data.at(0).size();k++) {
+            if(std::find(current_set_of_features.begin(),current_set_of_features.end(),k) == current_set_of_features.end()) {
+                accuracy = leave_one_out_cross_validation(data,current_set_of_features);
+            }
+                cout << "Using feature(s) {";
+                for(int x = 0; x < current_set_of_features.size(); ++x) {
+                    cout << current_set_of_features.at(x) << ", ";
+
+                }
+                cout << " } accuracy is " << accuracy << endl;
+                if(accuracy > best_so_far_accuracy) {
+                    best_so_far_accuracy = accuracy;
+                    feature_to_add_at_this_level = k;
+                }
+        }
+        current_set_of_features.push_back(feature_to_add_at_this_level); 
+    }           
+}
+
 int main() {
     string fileName; 
     ifstream fin; 
@@ -89,9 +121,16 @@ int main() {
 
     cout << "This dataset has " << num_features - 1 << " features (not including the class atribute), with " << num_instances << " instances " << endl << endl;
 
-    double accuracy = leave_one_out_cross_validation(data);
+    vector<int> all_features; 
+    for (int i = 1; i < data.at(0).size();i++) {
+        all_features.push_back(i);
+    }
+    double accuracy = leave_one_out_cross_validation(data,all_features);
     cout << "Running nearest neighbor with all " << num_features - 1 << " features, using leaving-one-out evaluation, I get an accuracy of " << accuracy << "%" << endl; 
 
+    cout << "Beginning search. " << endl << endl;
+
+    forward_Selection(data);
     // for (int i  = 0; i < data.size();i++) {
     //     for(int j = 0; j < data.at(i).size();j++) {
     //         cout << data.at(i).at(j) << endl;
